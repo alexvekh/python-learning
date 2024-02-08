9bea842a36e6fd2f314cf2373e5162af1648fc39
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2024-02-07 22:48:32 -0500

    5.4

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
-def sanitize_phone_number(phone):
-    return phone.strip().replace("-", "").replace("(", "").replace(")", "").replace("+", "").replace(" ", "")
-        
+# Приймати список телефонних номерів.
+# Санітизувати (нормалізувати) отриманий список телефонів клієнтів за допомогою нашої функції sanitize_phone_number.
+# Сортувати телефонні номери за вказаними у таблиці країнами.
+# Повертати словник зі списками телефонних номерів для кожної країни у такому вигляді:
 
-print(sanitize_phone_number("    +38(050)123-32-34"))
-print(sanitize_phone_number("     0503451234"))
-print(sanitize_phone_number("(050)8889900"))
-print(sanitize_phone_number("38050-111-22-22"))
-print(sanitize_phone_number("38050 111 22 11   "))
+def sanitize_phone_number(phone):
+    new_phone = (
+        phone.strip()
+        .removeprefix("+")
+        .replace("(", "")
+        .replace(")", "")
+        .replace("-", "")
+        .replace(" ", "")
+    )
+    return new_phone
+
+def get_phone_numbers_for_countries(list_phones):
+    
+    jp_phones = []
+    tw_phones = []
+    sg_phones = []
+    ua_phones = []
+    phone_map = {"UA": ua_phones,"JP": jp_phones, "TW": tw_phones, "SG": sg_phones}
+    
+    #sorted_phones = sorted(list_phones)
+    for duty_phone in list_phones:
+        phone = sanitize_phone_number(duty_phone)
+
+        if phone.startswith("81"):
+            jp_phones.append(phone)
+        elif phone.startswith("88"):
+            tw_phones.append(phone)
+        elif phone.startswith("65"):
+            sg_phones.append(phone)
+        else:
+            ua_phones.append(phone)
+
+    return phone_map
+
+# print(get_phone_numbers_for_countries(['380998759405', '657658976', '818765347', '8867658976']))
+# print(get_phone_numbers_for_countries(['(65)765-89-77', '(81)8765347', '065-875-94-11', '657658976', '8867658976']))
