36119fa4be9aff397b14db6db5f9cce801dfc580
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2024-02-23 09:15:06 -0500

    8 done

--- 8.py
+++ 8.py
@@ -176,3 +176,27 @@ def convert_list(cats):
 
 # print(convert_list(cats))
 # print(convert_list(cats2))
+#-------------------------------------------------------------------------------
+
+
+#7
+# Є список IP адрес: IP = ["85.157.172.253", ...]
+# Реалізуйте дві функції. 
+# Перша get_count_visits_from_ip за допомогою Counter повертатиме словник, 
+# де ключ це IP, а значення – кількість входжень у вказаний список.
+# Приклад: {'85.157.172.253': 2, ... }
+# Друга функція get_frequent_visit_from_ip повертає кортеж з найбільш часто 
+# уживаним в списку IP і кількістю його появ в списку.
+# Приклад: ('66.50.38.43', 4)
+
+from collections import Counter
+
+
+def get_count_visits_from_ip(ips):
+    return Counter(ips)
+
+def get_frequent_visit_from_ip(ips):
+    most_common = Counter(ips)
+    most_common_list = most_common.most_common(1)
+    # most_common_list = Counter(ips).most_common(1)
+    return most_common_list[0]
--- main.py
+++ main.py
@@ -1,49 +1,9 @@
-# У нас є іменований кортеж для зберігання котів у змінній Cat. 
-# На першому місці у нас кличка котика nickname, потім його вік age 
-# та ім'я власника кота owner.
-# Напишіть функцію convert_list(cats), яка працюватиме у двох режимах.
-# Якщо функція convert_list приймає у параметрі cats список іменованих кортежів
-# [Cat("Mick", 5, "Sara"), Cat("Barsik", 7, "Olga"), Cat("Simon", 3, "Yura")]
-# То функція поверне наступний список словників:
-# [
-#     {"nickname": "Mick", "age": 5, "owner": "Sara"},
-#     {"nickname": "Barsik", "age": 7, "owner": "Olga"},
-#     {"nickname": "Simon", "age": 3, "owner": "Yura"},
-# ]
-# І в той же час, якщо функція convert_list приймає в параметрі cats список словників, 
-# то результатом буде зворотна операція та функція поверне список іменованих кортежів.
-# Для визначення типу параметра cats використовуйте функцію isinstance.
-
-import collections
-
-Cat = collections.namedtuple("Cat", ["nickname", "age", "owner"])
-
-def is_map_list(list):
-    return all(isinstance(item, dict) for item in list)
-
-def is_tuple_list(list): 
-    return all(isinstance(item, tuple) for item in list)
-
-def convert_list(cats):
-    if is_map_list(cats):
-        new_list = []
-        for cat in cats:
-            new_list.append(Cat(nickname=cat["nickname"], age=cat['age'], owner=cat['owner']))
-
-    if is_tuple_list(cats):
-        new_list = []
-        for cat in cats:
-            new_list.append({"nickname": cat.nickname, "age": cat.age, "owner": cat.owner})
-
-    return new_list        
-
-# cats =  [Cat("Mick", 5, "Sara"), Cat("Barsik", 7, "Olga"), Cat("Simon", 3, "Yura")]
-# cats2 = [
-#     {"nickname": "Mick", "age": 5, "owner": "Sara"},
-#     {"nickname": "Barsik", "age": 7, "owner": "Olga"},
-#     {"nickname": "Simon", "age": 3, "owner": "Yura"},
-# ]
-
-# print(convert_list(cats))
-# print(convert_list(cats2))
-
+# На першому етапі вам потрібно реалізувати функцію для виведення списку 
+# колег, яких потрібно привітати з днем народження на тижні.
+# У вас є список словників users, кожен словник у ньому обов'язково має 
+# ключі name та birthday. Де name — це рядок з ім'ям користувача, 
+# а birthday — це datetime об'єкт, в якому записаний день народження.
+# Наприклад: {"name": "Bill Gates", "birthday": datetime(1955, 10, 28)}
+# Ваше завдання написати функцію get_birthdays_per_week, яка отримує на вхід 
+# список users і виводить у консоль (за допомогою print) список користувачів, 
+# яких потрібно привітати по днях на наступному тижні.
